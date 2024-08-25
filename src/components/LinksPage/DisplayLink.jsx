8e5105250d9b875bb04975245bd12411b4d7560c
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-08-26 01:30:04 +0530

    v1.0

--- src/components/LinksPage/DisplayLink.jsx
+++ src/components/LinksPage/DisplayLink.jsx
@@ -36,7 +36,7 @@ function DisplayLink(props){
         <>
         <span className="LinkBox">
             <div className="displayLink">
-                <a className="displayUrl">{props.url.replace(/(^\w+:|^)\/\//, '') || 'Loading...'}</a>
+                <a className="displayUrl" target="_blank" href={props.url}>{props.url.replace(/(^\w+:|^)\/\//, '') || 'Loading...'}</a>
                 <h5 style={{margin:'5px'}}>{props.running ? 'Running':'Paused'}</h5>
 
                 <div className="shape-circle" id={props.running ? (props.working ? 'green': 'red'):'grey'}> <span className="shape-circle-text"><h5 style={{margin:'1px'}}>status code: {props.status_code || 'N/A'}</h5></span> </div>
